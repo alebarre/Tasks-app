f79addabb1f2faebaebeba116b5f35f556391e4b
Alexandre Barreto <plotartemail@gmail.com>
2026-05-19 19:30:47 -0300

    Correção de CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,12 @@ import taskRoutes from './routes/task.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Rotas da API
